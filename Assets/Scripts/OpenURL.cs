cb92a828dc4448d53deffad02c80de823f5cee04
Abhijit Srikanth <abhijit@sketchup.com>
2020-03-19 12:52:50 -0600

    Edge browser bug on HoloLens 2 (EdgeTest scene)

---
+++ Assets/Scripts/OpenURL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public void Open(string uri)
+    {
+#if WINDOWS_UWP
+            UnityEngine.WSA.Application.InvokeOnUIThread(async () =>
+            {
+                bool result = await global::Windows.System.Launcher.LaunchUriAsync(new System.Uri(uri));
+                if (!result)
+                {
+                    Debug.LogError("Launching URI failed to launch.");
+                }
+            }, false);
+#else
+        Application.OpenURL(uri);
+#endif
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # UWP-EdgeTest-MRTK
+
+* Unity 2018.4.12f1
+* MRTK v2.3
+
+
+# Test Scenes
+
+## EdgeTest (Edge behaviours Bug)
+
+
+Steps to reproduce the behavior:
+
+1. Open EdgeTest Scene and run on HL2
+2. Click the button to open the URL
+3. Now click the button again, to open the URL again
+
+**Current bug**: After opening the URL for the second time, Edge is timed out
+
+**Expected behaviour**: Opens the URL seamlessly on the Edge browser
